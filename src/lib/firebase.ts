1659b60ec81b7aeb427f6ff21a84133c7e6f58bc
RafaelFS01 <rafafigs@outlook.com>
2025-04-09 00:41:38 -0300

    Firebase configurado.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,17 +5,19 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_your_api_key_here",
-  authDomain: "your-project-id.firebaseapp.com",
-  databaseURL: "https://your-project-id-default-rtdb.firebaseio.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyCafsB2vXkbIxoCBsKTUB72IE-ymw92KiQ",
+  authDomain: "catalog-generat.firebaseapp.com",
+  databaseURL: "https://catalog-generat-default-rtdb.firebaseio.com",
+  projectId: "catalog-generat",
+  storageBucket: "catalog-generat.firebasestorage.app",
+  messagingSenderId: "505433207966",
+  appId: "1:505433207966:web:1ea09e64c96e0337bf778f",
+  measurementId: "G-CP77G699V5"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 // Initialize Firebase services
 export const db = getDatabase(app);
